12bcc3a988cf6c172910b4d65ad4e78f89935ffa
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-07-20 15:08:01 +0530

    Update A3__visualMPEPChapterLengths.py

--- A3__visualMPEPChapterLengths.py
+++ A3__visualMPEPChapterLengths.py
@@ -1,8 +1,15 @@
 # PYTHON CODE
-# visualize the chapter lengths in minutes of MPEP chapters
+# visualize the chapter lengths in minutes of MPEP chapters (from PLI.edu)
 #
 #
 from bokeh.plotting import figure, output_file, show
+
+#
+# Chapter length in seconds of the lectures on the MPEP chapters in the 0's, 1000's and 2000's
+#
+# 1. MPEP 100-900
+# 2. MPEP 1000-1900
+# 3. MPEP 2000-2900
 fig = figure(title = "Chapter Lengths (MPEP 100-900, 1000's, 2000's)", plot_width = 375, plot_height = 175)
 fig.hbar(y = [000,1000,2000], height = 1, left = 0, right = [31060,10680,14933], color = "Red")
 output_file('bar.html')
